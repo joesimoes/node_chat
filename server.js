3d97b9a750b0e546689cf7490a12918e71fa6df8
Joe Simoes <joe@generalassemb.ly>
2014-11-22 17:38:15 -0500

    View setup and server updates

--- server.js
+++ server.js
@@ -11,7 +11,8 @@
 var express = require("express")
   , app = express()
   , http = require("http").createServer(app)
-  , bodyParser = require("body-parser");
+  , bodyParser = require("body-parser")
+  , _ = require("underscore");
 
 /* Server config */
 
@@ -21,6 +22,15 @@ app.set("ipaddr", "127.0.0.1");
 //Server's port number 
 app.set("port", 8080);
 
+//Specify the views folder
+app.set("views", __dirname + "/views");
+
+//View engine is Jade
+app.set("view engine", "jade");
+
+//Specify where the static content is
+app.use(express.static("public", __dirname + "/public"));
+
 //Tells server to support JSON requests
 app.use(bodyParser.json());
 
@@ -28,9 +38,25 @@ app.use(bodyParser.json());
 
 //Handle route "GET /", as in "http://localhost:8080/"
 app.get("/", function(request, response) {
+ 
+  //Render the view called "index"
+  response.render("index");
 
-  //Show a simple response message
-  response.send("Server is up and running");
+});
+
+//POST method to create a chat message
+app.post("/message", function(request, response) {
+
+  //The request body expects a param named "message"
+  var message = request.body.message;
+
+  //If the message is empty or wasn't sent it's a bad request
+  if(_.isUndefined(message) || _.isEmpty(message.trim())) {
+    return response.json(400, {error: "Message is invalid"});
+  }
+
+  //Looks good, let the client know
+  response.json(200, {message: "Message received"});
 
 });
 
